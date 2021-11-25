fd135ec73acfd3beaf830017f6d31fffe2fa6336
Tombo <91033804+TomboNic@users.noreply.github.com>
2021-11-25 11:12:53 -0300

    v.nada

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Hinchas(models.Model):
-    es_o_no = models.CharField(max_length=6)
+class FanFutbol(models.Model):
+    Nombre = models.CharField(max_length=15)
+    es_o_no = BooleanField()
+    equipo = models.CharField(max_length=15)
 
-class HinchasDeVelez(models.Model):
-    Cant_hinchas = models.IntegerField()
+class FanVoley(models.Model):
+    Nombre = models.CharField(max_length=15)
+    es_o_no = BooleanField()
     Equipo = models.CharField(max_length=15)
 
-class HinchasDeBoca(models.Model):
-    Cant_hinchas = models.IntegerField()
+class FanBasquet(models.Model):
+    Nombre = models.CharField(max_length=15)
+    es_o_no = BooleanField()
     Equipo = models.CharField(max_length=15)
 
-class HinchasDeRiber(models.Model):
-    Cant_hinchas = models.IntegerField()
-    Equipo = models.CharField(max_length=15)
+class FanFormula1(models.Model):
+    Nombre = models.CharField(max_length=15)
+    es_o_no = BooleanField()
+    Escuderia = models.CharField(max_length=15)
